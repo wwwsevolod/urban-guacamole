5fb4b50858148384ee63b3c535243d27c8e10f8c
Vsevolod Ivanov <www@sevolod.ru>
2016-07-03 23:17:15 +0300

    testing chronological order

--- test/createSubscriber.unit.js
+++ test/createSubscriber.unit.js
@@ -23,24 +23,50 @@ describe('createSubscriber', () => {
         expect(allReducerCallCount).equals(2);
     });
 
-    it('should subscribe to branch and be notified all levels to that branch, sorted right', () => {
+    it('should subscribe to branch and be notified all levels to that branch, called in chronological order', () => {
         const subscriber = createSubscriber();
+
+        let callCount = 0;
 
         let allReducerCallCount = 0;
         let firstReducerCallCount = 0;
         let anotherReducerCallCount = 0;
         let thirdReducerCallCount = 0;
 
-        let unsubscribeFromAll = subscriber.subscribe([[]], () => allReducerCallCount++);
-        let unsubscribeFromFirst = subscriber.subscribe([['first']], () => firstReducerCallCount++);
-        let unsubscribeFromAnother = subscriber.subscribe([['another']], () => anotherReducerCallCount++);
+        let unsubscribeFromAll = subscriber.subscribe([[]], () => {
+            allReducerCallCount++;
+            expect(callCount).equals(0);
+
+            callCount++;
+        });
+        
+        let unsubscribeFromAnother = subscriber.subscribe([['another']], () => {
+            anotherReducerCallCount++;
+        });
+
         let unsubscribeFromThird = subscriber.subscribe([[
             'first', 'second', 'third'
-        ]], () => thirdReducerCallCount++);
+        ]], () => {
+            thirdReducerCallCount++;
+
+            expect(callCount).equals(1);
+
+            callCount++;
+        });
+
+        let unsubscribeFromFirst = subscriber.subscribe([['first']], () => {
+            firstReducerCallCount++;
+
+            expect(callCount).equals(2);
+
+            callCount++;
+        });
 
         subscriber.emitChange([{
             path: ['first', 'second', 'third']
         }], []);
+
+        callCount = 0;
 
         expect(allReducerCallCount).equals(1);
         expect(firstReducerCallCount).equals(1);
